618dde3fb283244f8f456a4f31d48f4e2cf5ae15
zjk7-mosh <145497712+zjk7-mosh@users.noreply.github.com>
2023-09-29 18:43:02 -0400

    Filling in Date & Event classes

--- Project1/src/project1/Date.java
+++ Project1/src/project1/Date.java
@@ -1,8 +1,62 @@
 package project1;
-
+/** A class that represents the Date abstract data type with year, month, and day. ...*/
 public class Date implements Comparable<Date> {
     private int year;
     private int month;
     private int day;
     public boolean isValid() //check if the date is a valid calendar date
+
+    /** A parameterized constructor that takes a string in a form of "mm/dd/yyyy". ...*/
+    public Date(String date) {...}
+
+    /** A default constructor that creates a date with today's date. */
+    public Date() {...}
+
+    /** A copy constructor that clones a date object. ...*/
+    public Date(Date date) {...}
+
+    /** This method compares 2 dates. ...*/
+    @Override
+    public int compareTo(Date date) {...}
+
+    /** Return today's date. ...*/
+    public static Date today() { return new Date(); }
+
+    /** Check if this date object is a valid calendar date. ...*/
+    public boolean isValid() {...}
+
+    /** Return the textual representation of a fitness.student.Date object. ...*/
+    @Override
+    public String toString() { return month + "/" + day + "/" + year; }
+
+    /** Getter method for the day of a date. ...*/
+    public int getDay() { return day; }
+
+    /** Getter method for the month of a date ...*/
+    public int getMonth() { return month; }
+
+    /** Getter method for the year of a date. ...*/
+    public int getYear() { return year; }
+
+    /** Check if two dates are the same date. ...*/
+    @Override
+    public boolean equals(Object obj) {...}
+
+    /** Test case #1 */
+    private static void testDaysInFeb_Nonleap() {
+        Date date = new Date("2/29/2011"); //test data
+        boolean expectedOutput = false; //define expected output
+        boolean actualOutput = date.isValid(); //call the method to get the actual output
+        System.out.println("**Test case #1: # of days in Feb. in a non-leap year is 28");
+        testResult(date, expectedOutput, actualOutput); //compare the results
+    }
+
+    /** Test case #2 */
+    private static void testDaysInFeb_Leap() {...}
+    /** Test case #3 */
+    private static void testMonth_OutOfRange () {...}
+    /** Check if a given test case PASS or FAIL ...*/
+    private static void testResult(Date date, boolean expectedQutput, boolean actualOutput) {...}
+
+
 }
--- Project1/src/project1/Event.java
+++ Project1/src/project1/Event.java
@@ -1,13 +1,10 @@
 package project1;
-
+//bs: remove unused imports
 import java.util.Scanner;
 import java.util.StringTokenizer;
 import java.util.Calendar;
 import java.text.DecimalFormat;
-
-
-
-
+/** //bs: ADD Javadoc comments. */
 public class Event implements Comparable<Event> {
     private Date date; //the event date
     private Timeslot startTime; //the starting time
@@ -15,30 +12,54 @@ public class Event implements Comparable<Event> {
     private Contact contact; //include the department name and email
     private int duration; //in minutes
 
+    /** //bs: ADD COMMENTS. */
     public Event(Date date, Timeslot startTime, Location location, Contact contact, int duration) {
             this.date = date;
             this.startTime = startTime;
             this.location = location;
             this.contact = contact;
             this.duration = duration;
-        }
+    }
 
+    //bs: override equals method
+    /** //bs: ADD Javadoc comments. */
+    @Override
+    public boolean equals(Object obj) {
+        //returns true if two dates, timeslots and locations are equal
+        return super.equals(obj);
+    }
+
+    //bs: override toString method
+    /** //bs: ADD Javadoc comments. */
+    @Override
+    public String toString() {
+        //returns a textual representation of an event in the following format:
+        //"[Event Date: 10/21/2023] [Start: 2:00pm] [End: 3:00pm] @HLL114 (Hill Center, Busch) [Contact: Computer Science, cs@rutgers.edu]"
+        return super.toString();
+    }
+
+    //bs: implement compareTo method
+    /** //bs: ADD Javadoc comments. */
+    @Override
+    public int compareTo(Event o) {
+        //compares the dates first, then the timeslots if the dates are the same
+        return 0;
+    }
+
+    //IntelliJ Auto-generated getter methods bellow:
+      //bs: must remove unnecessary methods from below, and javadoc comment the kept ones before submit//
     public Date getDate() {
         return date;
     }
-
     public Timeslot getStartTime() {
         return startTime;
     }
-
     public Location getLocation() {
         return location;
     }
-
     public Contact getContact() {
         return contact;
     }
-
     public int getDuration() {
         return duration;
     }
