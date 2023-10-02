b8b974ac15727cc4b252171b6fb1b8d5710ace5a
zjk7-mosh <145497712+zjk7-mosh@users.noreply.github.com>
2023-10-02 19:44:12 -0400

    started EventOrganizer(barely)

--- Project1/src/project1/EventOrganizer.java
+++ Project1/src/project1/EventOrganizer.java
@@ -16,6 +16,26 @@ public class EventOrganizer {
      * Default constructor for EventOrganizer class.
      */
     public void run(){
+        //A while loop to continuously read the command lines from the console until a “Q” command is entered
+        while (true) {
+            //Read a command line from the console
+            Scanner sc = new Scanner(System.in);
+            String commandLine = sc.nextLine();
+            //If the command line is “Q”, then quit the program
+            if (commandLine.equals("Q")) {
+                break;
+            }
+            //If the command line is “A”, then add a new event to the calendar
+            else if (commandLine.equals("A")) {
+                //Read the event name, start time, and end time from the console
+                String eventName = sc.nextLine();
+                String startTime = sc.nextLine();
+                String endTime = sc.nextLine();
+                //Create a new event object with the given information
+                Event event = new Event(eventName, startTime, endTime);
+                //Add the event to the calendar
+
+
 
     }
 }
